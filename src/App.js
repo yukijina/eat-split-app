bcd168d224da740f4025a0ebe8a73070cf81d9ba
yuki <38411009+yukijina@users.noreply.github.com>
2026-01-05 17:01:25 -0800

    created components and export to the App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import FriendsList from './components/FriendsList';
 import './index.css';
+
+const initialFriends = [
+  {
+    id: 118836,
+    name: 'Clark',
+    image: 'https://i.pravatar.cc/48?u=118836',
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: 'Sarah',
+    image: 'https://i.pravatar.cc/48?u=933372',
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: 'Anthony',
+    image: 'https://i.pravatar.cc/48?u=499476',
+    balance: 0,
+  },
+];
 
 function App() {
   return (
-    <div className='App'>
-      <p>app</p>
+    <div className='app'>
+      <div className='sidebar'>
+        <FriendsList friends={initialFriends} />
+      </div>
     </div>
   );
 }
---
+++ src/components/FriendsList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Friend from './Friend';
+
+export default function FriendsList({ friends }) {
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
