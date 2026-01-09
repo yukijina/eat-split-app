717b545c72d7cef910dc05560cb1aa501c3bd0a5
yuki <38411009+yukijina@users.noreply.github.com>
2026-01-09 06:49:06 -0800

    updated button and class for the selected friend

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Button from './Button';
 
-export default function Friend({ friend }) {
+export default function Friend({ friend, selectedFriend, onSelectFriend }) {
+  const isSelected = selectedFriend?.id === friend.id;
+
   return (
-    <li>
+    <li className={isSelected ? 'selected' : ''}>
       <img src={friend.image} alt={friend.name} />
       <h3>{friend.name}</h3>
       {friend.balance > 0 && (
@@ -17,7 +19,9 @@ export default function Friend({ friend }) {
         </p>
       )}
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
-      <Button>Select</Button>
+      <Button onClick={() => onSelectFriend(friend)}>
+        {isSelected ? 'Close' : 'Select'}
+      </Button>
     </li>
   );
 }
