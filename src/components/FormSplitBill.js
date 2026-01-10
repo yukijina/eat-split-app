845f7d6a68288f7b6dd2590bb482aff1b4afb63b
yuki <38411009+yukijina@users.noreply.github.com>
2026-01-10 12:08:41 -0800

    add useState to control the form.

--- src/components/FormSplitBill.js
+++ src/components/FormSplitBill.js
@@ -1,30 +1,57 @@
+import { useState } from 'react';
+import Button from './Button';
+
 export default function FormSplitBill({ selectedFriend }) {
+  const [bill, setBill] = useState('');
+  const [userExpense, setUserExpense] = useState('');
+  const [payer, setPayer] = useState('user');
+  const friendExpense = bill && userExpense ? bill - userExpense : '';
+
+  function handleSplitBill() {}
+
   return (
     <form className='form-split-bill'>
       <h2>Split a bill with {selectedFriend.name}</h2>
 
       <div>
         <label htmlFor='bill'>💰 Bill value</label>
-        <input type='text' id='bill' />
+        <input
+          type='text'
+          id='bill'
+          value={bill}
+          onChange={(e) => setBill(Number(e.target.value))}
+        />
       </div>
       <div>
-        <label htmlFor='myExpense'>🧍‍♂️ Your expense</label>
-        <input type='text' id='myExpense' />
+        <label htmlFor='userExpense'>🧍‍♂️ Your expense</label>
+        <input
+          type='text'
+          id='userExpense'
+          value={userExpense}
+          onChange={(e) =>
+            setUserExpense(
+              Number(e.target.value) > bill
+                ? userExpense
+                : Number(e.target.value)
+            )
+          }
+        />
       </div>
       <div>
         <label htmlFor='friendsExpense'>
           👭 {selectedFriend.name}'s expense
         </label>
-        <input type='text' id='friendsExpense' disabled />
+        <input type='text' id='friendsExpense' disabled value={friendExpense} />
       </div>
+
       <div>
         <label htmlFor='who'>💸 Who is paying the bill?</label>
-        <select id='who'>
-          <option></option>
+        <select id='who' onChange={(e) => setPayer(e.target.value)}>
           <option value='user'>You</option>
           <option value='friend'>{selectedFriend.name}</option>
         </select>
       </div>
+      <Button>Split bill</Button>
     </form>
   );
 }
