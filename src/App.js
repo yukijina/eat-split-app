cc00fdd3fae36e171f0f37fc0b5baec2a86f3ef7
yuki <38411009+yukijina@users.noreply.github.com>
2026-01-29 06:24:16 -0800

    added key

--- src/App.js
+++ src/App.js
@@ -80,6 +80,7 @@ function App() {
         <FormSplitBill
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         />
       )}
     </div>
